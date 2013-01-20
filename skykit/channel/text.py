a4608a03f49d473ad5770f006b0e587f377fec70
Maksim Melnikau <maxposedon@gmail.com>
2013-01-20 04:38:05 +0300

    fix Channel implementation
    one-to-one chat should works now
    
    Signed-off-by: Maksim Melnikau <maxposedon@gmail.com>

--- skykit/channel/text.py
+++ skykit/channel/text.py
@@ -1,8 +1,6 @@
-from time import time
-from uuid import uuid4
-
 import gobject
 from dbus.types import Array, Dictionary, String, UInt32, UInt64
+from time import time
 
 from telepathy.constants import CHANNEL_TEXT_MESSAGE_TYPE_NORMAL
 from telepathy.server import ChannelTypeText, ChannelInterfaceMessages
@@ -19,50 +17,51 @@ class SkykitTextChannel(ChannelTypeText, ChannelInterfaceMessages):
     def __init__(self, conn, manager, props, object_path=None):
         _, surpress_handler, handle = manager._get_type_requested_handle(props)
         self.handle = handle
+        print "K", props, handle
         self.__message_received_id = 0
 
         ChannelTypeText.__init__(self, conn, manager, props, object_path)
         ChannelInterfaceMessages.__init__(self)
         self._skype_conversation = self._conn._skype.GetConversationByParticipants([handle.get_name()], True, True)
         print "E [%s]" % self._skype_conversation.identity
-        self._skype_conversation.OnMessage = self.OnMessage
 
     def SendMessage(self, message, flags):
-        token = str(uuid4())
-        print "D", list(message)
-        gobject.timeout_add(0, self._send_message, message, flags, token)
-        return token
+        skype_message = self._skype_conversation.PostText(message[1]['content'])
+        print "D [%s]", self._skype_conversation.identity, skype_message.guid.encode('hex'), skype_message.author, skype_message.timestamp, skype_message.body_xml
+        return skype_message.guid.encode('hex')
 
-    def _send_message(self, message, flags, token):
+    def _message_sent(self, skype_message):
         headers = Dictionary({
-            String('message-sent'): UInt64(time()),
+            String('message-sent'): UInt64(skype_message.timestamp),
             String('message-type'): UInt32(CHANNEL_TEXT_MESSAGE_TYPE_NORMAL),
         }, signature='sv')
         body = Dictionary({
             String('content-type'): String('text/plain'),
-            String('content'): message[1]['content'],
+            String('content'): skype_message.body_xml,
         }, signature='sv')
         message = Array([headers, body], signature='a{sv}')
-        self._skype_conversation.PostText(message[1]['content'], False)
-        self.MessageSent(message, flags, String(token))
+        print "G1", dict(message[0]), dict(message[1])
+        self.MessageSent(message, 0, String(skype_message.guid.encode('hex')))
 
-    def OnMessage(self, message):
-        print "F [%s]" % self._skype_conversation.identity, message.author, message.body_xml
-        if message.author != self._conn._skype_account.skypename:
-            self._message_received(message.body_xml)
+    def OnMessage(self, skype_message):
+        print "F [%s]", self._skype_conversation.identity, skype_message.guid.encode('hex'), skype_message.author, skype_message.timestamp, skype_message.body_xml
+        if skype_message.author == self._conn._skype_account.skypename:
+            gobject.timeout_add(100, self._message_sent, skype_message)
+        else:
+            gobject.timeout_add(100, self._message_received, skype_message)
 
-    def _message_received(self, msg):
+    def _message_received(self, skype_message):
         self.__message_received_id += 1
         header = Dictionary({
             'pending-message-id': UInt32(self.__message_received_id),
-            'message-received': UInt64(time()),
+            'message-received': UInt64(time()), #UInt64(skype_message.timestamp),
             'message-type': UInt32(CHANNEL_TEXT_MESSAGE_TYPE_NORMAL),
-            'sender-nickname': String(self.handle.get_name()),
+            'sender-nickname': String(skype_message.author),
             }, signature='sv')
         body = Dictionary({
             String('content-type'): String('text/plain'),
-            String('content'): String(msg),
+            String('content'): String(skype_message.body_xml),
         }, signature='sv')
         message = Array([header, body], signature='a{sv}')
-        print "G", list(message)
+        print "G2", dict(message[0]), dict(message[1])
         self.MessageReceived(message)
